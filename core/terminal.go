ec6ede4c7c9663ec52c0a1d74f526e36d06cfbe0
Japheth Obala <japhethobalak@gmail.com>
2019-11-30 13:12:54 +0300

    Controls terminal's cursor position

--- core/terminal.go
+++ core/terminal.go
@@ -2,12 +2,24 @@ package core
 
 import "fmt"
 
+type Terminal struct {
+	CursorX int
+	CursorY int
+}
+
 var Esc = "\x1b"
 
 func escape(format string, args ...interface{}) string {
 	return fmt.Sprintf("%s%s", Esc, fmt.Sprintf(format, args...))
 }
 
-func MoveCursorTo(line, col int) string {
+func (terminal *Terminal) MoveCursorTo(line, col int) string {
+	terminal.CursorX = line
+	terminal.CursorY = col
 	return escape("[%d;%dH", line, col)
+}
+
+func (terminal *Terminal) Write(msg string) {
+	escaped := escape("[%d;%dH", terminal.CursorX, terminal.CursorY)
+	fmt.Print(escaped, msg)
 }
