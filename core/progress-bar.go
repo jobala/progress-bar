f11c1c24de6ece2d9f9cbea0e8c4205593de6545
Japheth Obala <japhethobalak@gmail.com>
2019-11-30 13:11:48 +0300

    Renders & Updates progress bar

--- core/progress-bar.go
+++ core/progress-bar.go
@@ -1,30 +1,71 @@
 package core
 
 import (
-	"go/types"
 	"time"
 )
 
 type ProgressBar struct {
 	value           int
 	total           int
-	payload         types.Nil
 	startTime       time.Time
-	lastDrawnString types.Nil
+	lastDrawnString string
 	active          bool
 	lasRedraw       time.Time
+	terminal        Terminal
 }
 
 func NewProgressBar() ProgressBar {
 	return ProgressBar{
 		value:           0,
 		total:           100,
-		payload:         nil,
-		startTime:       nil,
-		lastDrawnString: nil,
+		startTime:       time.Now(),
+		lastDrawnString: "",
 		active:          false,
 		lasRedraw:       time.Now(),
+		terminal:        Terminal{0, 0},
 	}
+}
+
+func (progressBar *ProgressBar) Start(total int, startValue int) {
+	progressBar.value = startValue
+	progressBar.total = total
+
+	progressBar.startTime = time.Now()
+	progressBar.lastDrawnString = ""
+
+	progressBar.active = true
+
+	progressBar.Render()
+
+	for progressBar.value < progressBar.total {
+		progressBar.increment(10)
+		time.Sleep(time.Second)
+		progressBar.Render()
+	}
+}
+
+func (progressBar *ProgressBar) Render() {
+	config := GetConfig()
+	s := Format(progressBar, config)
+
+	if progressBar.lastDrawnString != s {
+		progressBar.terminal.MoveCursorTo(2, 0)
+		progressBar.terminal.Write(s)
+		progressBar.lastDrawnString = s
+		progressBar.lasRedraw = time.Now()
+	}
+}
+
+func (progressBar *ProgressBar) Update(current int) {
+	progressBar.value = current
+
+	if progressBar.value >= progressBar.getTotal() {
+		progressBar.stop()
+	}
+}
+
+func (progressBar *ProgressBar) increment(step int) {
+	progressBar.Update(progressBar.value + step)
 }
 
 func (progressBar *ProgressBar) setTotal(total int) {
