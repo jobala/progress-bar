46cb971f4b2a9c8047f2fe7c409a3b1538b6cdf2
Japheth Obala <japhethobalak@gmail.com>
2019-11-30 13:13:38 +0300

    Uses the progress bar object

--- main.go
+++ main.go
@@ -2,18 +2,9 @@ package main
 
 import (
 	"./core"
-	"fmt"
-	"time"
 )
 
 func main() {
-	i := 0
-	col := 0
-
-	for i < 3 {
-		fmt.Print(core.MoveCursorTo(2, col), "Hello world")
-		time.Sleep(2 * time.Second)
-		col += 15
-		i++
-	}
+	bar := core.NewProgressBar()
+	bar.Start(100, 0)
 }
